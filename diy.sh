1ae16e570e08cb16107d6a3f72cc2a222b56d2f6
boatmac <16136257+boatmac@users.noreply.github.com>
2020-02-25 23:59:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+./scripts/feeds update -a
+./scripts/feeds install -a
+git clone https://github.com/Lienol/openwrt-package package/Lienol
+sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
