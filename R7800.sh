a89d229c371e1134eac634455cddc6467fadd970
boatmac <16136257+boatmac@users.noreply.github.com>
2020-02-26 00:11:22 +0800

    Create R7800.sh

---
+++ R7800.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+git clone https://github.com/Lienol/openwrt-package package/Lienol
+./scripts/feeds update -a
+./scripts/feeds install -a
+sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
