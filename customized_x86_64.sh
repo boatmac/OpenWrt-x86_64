ff136ecbb4ef71325af179e852a31120b3755516
boatmac <16136257+boatmac@users.noreply.github.com>
2020-02-26 16:07:03 +0800

    Update customized_x86_64.sh

--- customized_x86_64.sh
+++ customized_x86_64.sh
@@ -10,17 +10,17 @@
 echo src-git lienol https://github.com/Lienol/openwrt-package >> feeds.conf.default
 ./scripts/feeds clean
 ./scripts/feeds update -a
-rm -rf feeds/lienol/lienol/ipt2socks
-rm -rf feeds/lienol/lienol/shadowsocksr-libev
-rm -rf feeds/lienol/lienol/pdnsd-alt
-rm -rf feeds/lienol/package/verysync
-rm -rf feeds/lienol/lienol/luci-app-verysync
-rm -rf package/lean/kcptun
-rm -rf package/lean/trojan
-rm -rf package/lean/v2ray
-rm -rf package/lean/luci-app-kodexplorer
-rm -rf package/lean/luci-app-pppoe-relay
-rm -rf package/lean/luci-app-pptp-server
-rm -rf package/lean/luci-app-v2ray-server
+#rm -rf feeds/lienol/lienol/ipt2socks
+#rm -rf feeds/lienol/lienol/shadowsocksr-libev
+#rm -rf feeds/lienol/lienol/pdnsd-alt
+#rm -rf feeds/lienol/package/verysync
+#rm -rf feeds/lienol/lienol/luci-app-verysync
+#rm -rf package/lean/kcptun
+#rm -rf package/lean/trojan
+#rm -rf package/lean/v2ray
+#rm -rf package/lean/luci-app-kodexplorer
+#rm -rf package/lean/luci-app-pppoe-relay
+#rm -rf package/lean/luci-app-pptp-server
+#rm -rf package/lean/luci-app-v2ray-server
 ./scripts/feeds install -a
 sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
