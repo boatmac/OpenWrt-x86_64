a8519f8fe3383588fbcc016e33bdd6692477c705
boatmac <16136257+boatmac@users.noreply.github.com>
2020-05-23 01:36:26 +0800

    Update customizedA_x86_64.sh

--- customizedA_x86_64.sh
+++ customizedA_x86_64.sh
@@ -12,6 +12,3 @@ sed -i 's/#src-git helloworld/src-git helloworld/g' feeds.conf.default
 
 # Add additional source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
