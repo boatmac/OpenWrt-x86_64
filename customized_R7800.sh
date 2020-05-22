fae465138b5613b3e85bd0c6280ae0d4fda36350
boatmac <16136257+boatmac@users.noreply.github.com>
2020-05-23 01:42:36 +0800

    Update customized_R7800.sh

--- customized_R7800.sh
+++ customized_R7800.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
-#=================================================
+#=============================================================
 # Enable feature
 sed -i 's/#src-git helloworld/src-git helloworld/g' feeds.conf.default
 
 # Add additional source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
