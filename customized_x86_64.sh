b5b022a81c65fbf2c9966e99c277623d7d708983
boatmac <16136257+boatmac@users.noreply.github.com>
2020-05-23 01:29:17 +0800

    Update customized_x86_64.sh

--- customized_x86_64.sh
+++ customized_x86_64.sh
@@ -5,11 +5,11 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-#git clone https://github.com/Lienol/openwrt-package package/Lienol
-#echo src-git lienol https://github.com/Lienol/openwrt-package >> feeds.conf.default
+# Enable feature
 sed -i 's/#src-git helloworld/src-git helloworld/g' feeds.conf.default
-./scripts/feeds clean
-./scripts/feeds update -a
-./scripts/feeds install -a
+
+# Add additional source
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Modify default IP
 sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
